ec213b0580723efb5e8874f510c7c1dcbdea4155
Collin Van der Vorst <collin@littlemissrobot.com>
2021-12-07 10:20:01 +0100

    Add styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Navigation component -->
+    <nav class="c-nav">
+      <ul class="c-nav-list">
+        <li class="c-nav-list-item active">
+          Home
+        </li>
+
+        <li class="c-nav-list-item">
+          Voting
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Alerts -->
+    <div class="c-alert">
+      Woops something went wrong
+    </div>
+
+    <div class="c-alert warning">
+      Warning warning, you should pay attention
+    </div>
+
+    <div class="c-alert succes">
+      Great Success
+    </div>
+
+    <div class="c-alert neutral">
+      Great Success
+    </div>
+
+    <!-- Main content -->
+    <h1>
+      Eurosong festival
+    </h1>
+
+    <p>
+      This is our voting application
+    </p>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import style from "./scss/style.scss";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
